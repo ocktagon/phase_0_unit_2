045b888b3712f7a2d1223c1d9ad8079f508136d3
Aki Suzuki <akisuzucky@gmail.com>
2014-04-15 00:17:13 -0700

    1 COMPLETE, 2 WIP

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W6: Testing Assert Statements
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # 2. Review the simple assert statement
@@ -10,23 +10,44 @@ def assert
   raise "Assertion failed!" unless yield
 end
  
-name = "bettysue"
-assert { name == "bettysue" }
-assert { name == "billybob" }
+# name = "bettysue"
+# assert { name == "bettysue" }
+# assert { name == "billybob" }
 
 # 2. Pseudocode what happens when the code above runs
-
+# When method asset runs and compares to name "bettysue"
+# IF name == "bettysue", then return nil
+# IF name == "billybob" then return nil
+#   else 
+#     raise a RunTime error with message "Assertion failed!"
+#   	p line of code where assert statement that returned false was defined
+#   	p line of code returning false
+#   	Location of Ruby where the issue is occurring (i.e. <main>)
 
 
 # 3. Copy your selected challenge here
 
 
 
+# puts boggle_board[0][1] == "r"   # returns boggle_board[row_number][column_number]
+# puts boggle_board[2][1] == "c" 
+# puts boggle_board[3][3] == "e" 
+# puts boggle_board[2][3] == "x" 
+
+
 
 # 4. Convert your driver test code from that challenge into Assert Statements
 
-
+assert { boggle_board[0][1] == "r" }
+assert { boggle_board[2][1] == "c" }
+assert { boggle_board[3][3] == "e" }
+assert { boggle_board[2][3] == "x" }
 
 
 
 # 5. Reflection 
+This was a very straightforward exercise.  So it seems like 
+it's relatively easy to create a specific method for testing purposes, 
+and wrap specific tests with the method to make tests more handy.  
+I would assume that this would be much more scalable to add more detail to the test during development. 
+
--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,80 +1,91 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
 
-# Are there any more methods needed in this class?
+	def initialize
+		@contents = []
+		@open = true
+	end
 
-def initialize
-@contents = []
-@open = true
-end
+	def open
+		@open = true
+	end
 
-def open
-@open = true
-end
+	def close
+		@open = false
+	end 
 
-def close
-@open = false
-end 
+	def add_item(item)
+		@contents << item
+	end
 
-def add_item
-@contents << item
-end
+	def remove_item(item = @contents.pop) #what is `#pop` doing?
+		@contents.delete(item)
+	end
 
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
+	def dump  # what should this method return?
+		puts "Your drawer is empty."
+	end
 
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
+	def view_contents
+	  puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end
 end
 
 
 class Silverware
-attr_reader :type
 
-# Are there any more methods needed in this class?
+	attr_reader :type
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
+
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+	end
+
+	def clean_silverware
+		@clean = true
+	end
+
 end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
-end
-
+  def assert
+  	raise "Assertion failed!" unless yield
+	end
+ 
+# name = "bettysue"
+# assert { name == "bettysue" }
+# assert { name == "billybob" }
 knife1 = Silverware.new("knife")
+spoon1 = Silverware.new("spoon")
+fork1 = Silverware.new("fork")
 
 silverware_drawer = Drawer.new
+
 silverware_drawer.add_item(knife1) 
-silverware_drawer.add_item(Silverware.new("spoon"))
-silverware_drawer.add_item(Silverware.new("fork")) 
+silverware_drawer.add_item(spoon1)
+silverware_drawer.add_item(fork1) 
 silverware_drawer.view_contents
 
-silverware_drawer.remove_item
+# silverware_drawer.remove_item
 silverware_drawer.view_contents
+
 sharp_knife = Silverware.new("sharp_knife")
 
-
 silverware_drawer.add_item(sharp_knife)
-
 silverware_drawer.view_contents
 
 removed_knife = silverware_drawer.remove_item(sharp_knife)
@@ -86,11 +97,11 @@ silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
 
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
+removed_fork = silverware_drawer.remove_item(fork1) #add some puts statements to help you trace through the code...
+removed_fork.eat
 
 #BONUS SECTION
-# puts fork.clean
+puts removed_fork.clean_silverware
 
 # DRIVER TESTS GO BELOW THIS LINE
 
