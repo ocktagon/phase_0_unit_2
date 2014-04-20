33787937c13470d15fb266ed5ce8a7628026d701
Aki Suzuki <akisuzucky@gmail.com>
2014-04-19 23:59:40 -0700

    4 and 5

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -1,20 +1,86 @@
 # U2.W6: Create a Playlist from Driver Code
 
 
-# I worked on this challenge [by myself, with: ].
-
+# I worked on this challenge [by myself].
 
 # Pseudocode
-
-
+# Class named Song 
+#   CREATE method initialize with arguments for title, author
+#   CREATE method play
+#     return array.first
+#   sub class named Playlist
+#     CREATE method initialize which takes many song var names
+#       Initialize playlist as empty array
+#       PUSH many song var names into playlist
+#     CREATE method add which takes many song var names
+#       PUSH many song names into playlist
+#     CREATE method num_of_tracks
+#       return count of array length
+#     CREATE method remove, take one var argument
+#       DELETE var from array
+#     CREATE method includes?, take one string
+#       return true if array includes string
+#     CREATE method play_all
+#       return array
+#     CREATE method display
+#       return array
+#   END sub class
+# END class
 
 # Initial Solution
 
+# Class named Song 
+
+class Song
+  attr_accessor :title, :artist
+
+
+	def initialize(title, artist)
+		@title = title
+		@artist = artist
+	end
+
+	def play
+		puts "Playing: " + "#{title}" + ", " + "#{artist}"
+	end
+end
+
+class Playlist
+  attr_accessor :playlist
+  def initialize(*song)
+  	@playlist = []
+    song.each{ |s| playlist << s }
+  end
+  def add(*song)
+    song.each{ |s| playlist << s }
+  end
+
+  def num_of_tracks
+  	playlist.length 
+  end
+
+  def remove(song)
+    playlist.delete(song)
+  end
+
+  def includes?(string)
+  	return true if playlist.include?(string)
+  end
+
+  def play_all
+  	playlist.each {|s| s.play} 
+  end
+
+  def display
+  	p playlist
+  end
+
+end
 
 
 
 # Refactored Solution
-
+# I already refactored my solution on the first stab. 
 
 
 
@@ -43,3 +109,11 @@ my_playlist.display
 
 
 # Reflection 
+# This was a great exercise in trying to master one-liner code.  
+# Unless there is secret trick to create an array and add things to it in one-line
+# I don't think the code could get any more efficient. 
+
+# I had the hardest time trying to understand what to do with the 
+# playlist method since the instances that were displayed in the array didn't 
+# make sense to me at first, but then I realized that I could simply call single play on every one that I defined in the 
+# earlier Song class.  
--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -1,26 +1,74 @@
 # U2.W6: Refactoring for Code Readability
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Original Solution
+# Borrowed jpabico's code!
+
+# class CreditCard
+
+# 	def initialize(ccnum)
+# 		@ccnum = ccnum
+# 		raise ArgumentError.new("There must be exactly 16 digits in a credit card number") if ccnum.to_s.length != 16 
+# 	end
+
+# 	def check_card
+# 		ccarray = @ccnum.to_s.split("")
+# 		ccarray.map! { |x| x.to_i }
+
+# 		for i in 0...ccarray.length
+# 			ccarray[i] = ccarray[i] * 2 if i%2 == 0
+# 		 	ccarray[i] = ccarray[i]/10 + ccarray[i]%10 if ccarray[i] >=10
+# 		end
+
+# 		ccarray.inject(:+) % 10 == 0 ? true : false	
+# 	end
+
+# end
 
 
 
 
 # Refactored Solution
 
+class CreditCard
 
+	def initialize(ccnum)
+		@ccnum = ccnum
+		raise ArgumentError.new("There must be exactly 16 digits in a credit card number") if ccnum.to_s.length != 16 
+	end
 
+	def check_card
+		ccarray = @ccnum.to_s.split("").map! { |x| x.to_i }
+
+		ccarray.each {|i|
+			i = i * 2 if i % 2 == 0
+			i = i / 10 + i % 10 if i >=10
+		}
+		
+		ccarray.inject(:+) % 10 == 0 ? true : false	
+	end
+
+end
 
 
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+test_card_1 = CreditCard.new(8651243992187311)
+p test_card_1.check_card == true
 
+test_card_2 = CreditCard.new(4545454545454545)
+p test_card_2.check_card == false
 
 
 
 
 # Reflection 
+# Jpabico's code was already super efficient so didn't really have much to 
+# refactor.  I did two things - make the first alteration (split + map) of ccarray into one line. 
+# I also changed the for/in statement to an each do statement that basically does
+# 	the same thing.  In essence the code has not changed but maybe it's a bit more readable?
+# Highly subjective though, as the code before refactoring looked fine too. 
